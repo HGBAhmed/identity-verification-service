422a74de810fea6aa92e7c5b32c8e73452c7e88c
Ahmed <ahmedgoniboulama@gmail.com>
2025-07-09 12:16:16 +0200

    task : microservice pour la detection de visage et l'extraction de données #124347
    -ajout de scripts pour le démarrage
    -modification des configurations
    user: ahmed
    version: 1.0.0

---
+++ start.ps1
@@ -0,0 +1,8 @@
+﻿Get-Content .env | ForEach-Object {
+    if ($_ -match "^([^#][^=]+)=(.*)$") {
+        $name = $matches[1].Trim()
+        $value = $matches[2].Trim()
+        [Environment]::SetEnvironmentVariable($name, $value, "Process")
+    }
+}
+./gradlew run
